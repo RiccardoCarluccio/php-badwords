a53f5d2900adc44c33c6fe0cb1332b09e09a0aa1
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-09-06 17:01:17 +0200

    end task

--- filter.php
+++ filter.php
@@ -1,1 +1,26 @@
-<h1>test</h1>
+<?php
+  $badWord = $_POST["bad-word"];
+  $text = $_POST["text"];
+  $censor = str_replace($badWord, "***", $text);
+
+  $textLength = strlen($text);
+  $censorLength = strlen($censor);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>PHP Bad Word #2</title>
+</head>
+<body>
+  <div class="container">
+    <p><span>Text wrote:</span> <?php echo $text ?></p>
+    <p><span>Word to censor:</span> <?php echo $censor ?></p>
+    <p><span>Text length:</span> <?php echo $textLength ?></p>
+    <p><span>Text length after censor:</span> <?php echo $censorLength ?></p>
+  </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,23 +1,22 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
-  <title>Document</title>
+  <title>PHP Bad Word #1</title>
 </head>
 <body>
   <div class="apex">
-    <form action="filter.php" method="POST">
-      <input type="text" name="bad-word" placeholder="bad word">
+    <div class="container">
+      <form action="filter.php" method="POST">
+        <input type="text" name="bad-word" placeholder="bad word">
 
-      <input type="text" name="text" placeholder="text to check">
-      <button type="submit">submit</button>
-    </form>
+        <input type="text" name="text" placeholder="text to check">
+        <button type="submit">submit</button>
+        <button type="reset">clear text</button>
+      </form>
+    </div>
   </div>
 </body>
 </html>
