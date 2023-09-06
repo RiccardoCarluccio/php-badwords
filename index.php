f3166a47a57893e711870b6e70da8af69cbf6c26
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-09-06 16:38:38 +0200

    create input

--- index.php
+++ index.php
@@ -7,9 +7,17 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <title>Document</title>
 </head>
 <body>
-  
+  <div class="apex">
+    <form action="filter.php" method="POST">
+      <input type="text" name="bad-word" placeholder="bad word">
+
+      <input type="text" name="text" placeholder="text to check">
+      <button type="submit">submit</button>
+    </form>
+  </div>
 </body>
 </html>
